1c600d38297661ede45349cfe50a79452f0b2772
sax <s.apostolico@gmail.com>
2015-10-27 15:05:00 +0100

    Store Pelican site

--- pelicanconf.py
+++ pelicanconf.py
@@ -31,21 +31,24 @@ TIMEZONE = 'Europe/Rome'
 DEFAULT_LANG = u'en'
 
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
+# FEED_ALL_ATOM = None
+# CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+TAG_FEED_ATOM = 'feeds/tags/%s.atom.xml'
+
 # FEED_ALL_ATOM = 'feeds/all.atom.xml'
 # CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
 
-FEED_RSS = 'feeds/%s.rss.xml'
+# FEED_RSS = 'feeds/%s.rss.xml'
 FEED_ALL_RSS = 'feeds/all.rss.xml'
-# AUTHOR_FEED_RSS = 'feeds/%s.sax.rss.xml'
+CATEGORY_FEED_RSS= 'feeds/category/%s.rss.xml'
 TAG_FEED_RSS = 'feeds/tags/%s.rss.xml'
-# CATEGORY_FEED_RSS= 'feeds/category/%s.rss.xml'
 FEED_MAX_ITEMS=5
 
 # AUTHOR_FEED_RSS = 'feeds/%s.rss.xml'
